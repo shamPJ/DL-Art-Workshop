16ec80c92dc08d93e8ac70f0214f1172579aa7b7
Shamsi Abdurakhmanova <sham.abdurakh@gmail.com>
2022-04-13 11:44:16 +0300

    add notebooks

---
+++ notebooks/DeepDream/utils.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib as mpl
+import IPython.display as display
+import PIL.Image   
+
+# Compute Loss
+def calc_loss(img, model):
+  # Pass forward the image through the model to retrieve the activations.
+  # Converts the image into a batch of size 1.
+  img_batch = tf.expand_dims(img, axis=0)
+  layer_activations = model(img_batch)
+  if len(layer_activations) == 1:
+    layer_activations = [layer_activations]
+
+  losses = []
+  for act in layer_activations:
+    loss = tf.math.reduce_mean(act)
+    losses.append(loss)
+
+  return  tf.reduce_sum(losses)
+
+# Shift image 
+def random_roll(img, maxroll):
+  # Randomly shift the image to avoid tiled boundaries.
+  shift = tf.random.uniform(shape=[2], minval=-maxroll, maxval=maxroll, dtype=tf.int32)
+  img_rolled = tf.roll(img, shift=shift, axis=[0,1])
+  return shift, img_rolled
+
+# Gradient ascent
+class DeepDream(tf.Module):
+  def __init__(self, model):
+    self.model = model
+
+  @tf.function(
+      input_signature=(
+        tf.TensorSpec(shape=[None,None,3], dtype=tf.float32),
+        tf.TensorSpec(shape=[], dtype=tf.int32),
+        tf.TensorSpec(shape=[], dtype=tf.float32),)
+  )
+  def __call__(self, img, steps, step_size):
+      print("Tracing")
+      loss = tf.constant(0.0)
+      for n in tf.range(steps):
+        with tf.GradientTape() as tape:
+          # This needs gradients relative to `img`
+          # `GradientTape` only watches `tf.Variable`s by default
+          tape.watch(img)
+          loss = calc_loss(img, self.model)
+
+        # Calculate the gradient of the loss with respect to the pixels of the input image.
+        gradients = tape.gradient(loss, img)
+
+        # Normalize the gradients.
+        gradients /= tf.math.reduce_std(gradients) + 1e-8 
+        
+        # In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
+        # You can update the image by directly adding the gradients (because they're the same shape!)
+        img = img + gradients*step_size
+        img = tf.clip_by_value(img, -1, 1)
+
+      return loss, img
+
+
+# Gradient for tiles of an image
+class TiledGradients(tf.Module):
+  def __init__(self, model):
+    self.model = model
+
+  @tf.function(
+      input_signature=(
+        tf.TensorSpec(shape=[None,None,3], dtype=tf.float32),
+        tf.TensorSpec(shape=[2], dtype=tf.int32),
+        tf.TensorSpec(shape=[], dtype=tf.int32),)
+  )
+  def __call__(self, img, img_size, tile_size=512):
+    shift, img_rolled = random_roll(img, tile_size)
+
+    # Initialize the image gradients to zero.
+    gradients = tf.zeros_like(img_rolled)
+    
+    # Skip the last tile, unless there's only one tile.
+    xs = tf.range(0, img_size[1], tile_size)[:-1]
+    if not tf.cast(len(xs), bool):
+      xs = tf.constant([0])
+    ys = tf.range(0, img_size[0], tile_size)[:-1]
+    if not tf.cast(len(ys), bool):
+      ys = tf.constant([0])
+
+    for x in xs:
+      for y in ys:
+        # Calculate the gradients for this tile.
+        with tf.GradientTape() as tape:
+          # This needs gradients relative to `img_rolled`.
+          # `GradientTape` only watches `tf.Variable`s by default.
+          tape.watch(img_rolled)
+
+          # Extract a tile out of the image.
+          img_tile = img_rolled[y:y+tile_size, x:x+tile_size]
+          loss = calc_loss(img_tile, self.model)
+
+        # Update the image gradients for this tile.
+        gradients = gradients + tape.gradient(loss, img_rolled)
+
+    # Undo the random shift applied to the image and its gradients.
+    gradients = tf.roll(gradients, shift=-shift, axis=[0,1])
+
+    # Normalize the gradients.
+    gradients /= tf.math.reduce_std(gradients) + 1e-8 
+
+    return gradients 
+
